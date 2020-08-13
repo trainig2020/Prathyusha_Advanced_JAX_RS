1c1b2108dc6012716a17a79381feaf4cfbcc158a
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-14 04:00:58 +0530

    Add files via upload

---
+++ Advanced_jaxrs_POST_Request/src/main/java/org/demo/rest/ShortDateMessageBodyWriter.java
@@ -0,0 +1,42 @@
+package org.demo.rest;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Date;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Produces("text/shortdate")
+public class ShortDateMessageBodyWriter implements MessageBodyWriter<Date> {
+	
+	
+	@Override
+	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+		return Date.class.isAssignableFrom(type);
+	
+	}
+
+	@Override
+	public long getSize(Date t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+		return -1;
+	}
+
+	@Override
+	public void writeTo(Date date, Class<?> type, Type type1, Annotation[] antns, MediaType mt,
+			MultivaluedMap<String, Object> mm, OutputStream out)
+			throws IOException, WebApplicationException {
+		String shortDate = date.getDate() + "-" + date.getMonth() + "-" + date.getYear();
+		out.write(shortDate.toString().getBytes());
+		
+	}
+
+
+}
