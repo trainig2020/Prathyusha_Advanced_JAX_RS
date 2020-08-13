9d13db31fbbafc3bb29b17b03f58591341858a1c
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-14 04:02:07 +0530

    Add files via upload

---
+++ Advanced_jaxrs_MsgBodyReader_Writer/src/main/java/org/demo/rest/MyResource.java
@@ -0,0 +1,22 @@
+package org.demo.rest;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+public class MyResource {
+
+	    @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public Date testMethod() {
+	      return Calendar.getInstance().getTime();
+        }
+	
+}
